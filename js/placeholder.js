048603ea6d3fd01f2b7c36896b42bae5ee25b7a0
Jarrett Meyer <jarrettmeyer@gmail.com>
2012-06-27 09:42:22 -0300

    passes jslint

--- js/placeholder.js
+++ js/placeholder.js
@@ -1,73 +1,75 @@
 function applyPlaceholders() {
-    if (Modernizr == undefined || Modernizr.input == undefined)
-        throw "Unable to find Modernizr library."
-
-    this.hasNativePlaceholderSupport = Modernizr.input.placeholder;
-    this.hasAppliedFallback = false;
-    this.placeholders = $("[placeholder]");
-    this.numberOfPlaceholderElements = this.placeholders.length;
-
-    if (!this.hasNativePlaceholderSupport) {        
-        applyPlaceholdersOnLoad();
-        handleFocusAndBlurEvents();
-        removePlaceholdersOnSubmit();
-        this.hasAppliedFallback = true;        
+    "use strict";
+    if (Modernizr === undefined || Modernizr.input === undefined) {
+        throw "Unable to find Modernizr library.";
     }
+
+    var hasNativePlaceholderSupport = Modernizr.input.placeholder,
+        hasAppliedFallback = false,
+        placeholders = $("[placeholder]");
 
     function addPlaceholderClass($input) {
-        this.placeholders.addClass('placeholder');
-    }
-    function applyPlaceholdersOnLoad() { 
-        this.placeholders.each(function() {
-            var $this = $(this);
-            if (isValueEmpty($this) || isValueEqualToPlaceholder($this)) {                                
-                setValueEqualToPlaceholder($this); 
-                addPlaceholderClass($this);           
-            }
-        });
-    }
-    function clearValue($input) {
-        return $input.val('');
-    }
-    function handleFocusAndBlurEvents() {
-        this.placeholders.focus(function() {
-            var $this = $(this);
-            if (isValueEqualToPlaceholder($this)) {
-                clearValue($this);
-                removePlaceholderClass($this);                
-            }
-        }).blur(function() {
-            var $this = $(this);
-            if (isValueEmpty($this) || isValueEqualToPlaceholder($this)) {
-                setValueEqualToPlaceholder($(this));
-                addPlaceholderClass($this);                
-            }
-        });
-    }
-    function isValueEmpty($input) {
-        return $input.val() == undefined || $input.val() == '';
-    }
-    function isValueEqualToPlaceholder($input) { 
-        return $input.val() == $input.attr('placeholder');
+        $input.addClass('placeholder');
     }
     function removePlaceholderClass($input) {
         $input.removeClass($input);
     }
-    function removePlaceholdersOnSubmit() {
-        this.placeholders.closest('form').submit(function() {
+    function clearValue($input) {
+        return $input.val('');
+    }
+    function isValueEmpty($input) {
+        return $input.val() === undefined || $input.val() === '';
+    }
+    function isValueEqualToPlaceholder($input) {
+        return $input.val() === $input.attr('placeholder');
+    }
+    function setValueEqualToPlaceholder($input) {
+        var placeholderValue = $input.attr('placeholder');
+        $input.val(placeholderValue);
+    }
+    function applyPlaceholdersOnLoad() {
+        placeholders.each(function () {
             var $this = $(this);
-            $this.find('[placeholder]').each(function() {
+            if (isValueEmpty($this) || isValueEqualToPlaceholder($this)) {
+                setValueEqualToPlaceholder($this);
+                addPlaceholderClass($this);
+            }
+        });
+    }
+    function handleFocusAndBlurEvents() {
+        placeholders.focus(function () {
+            var $this = $(this);
+            if (isValueEqualToPlaceholder($this)) {
+                clearValue($this);
+                removePlaceholderClass($this);
+            }
+        }).blur(function () {
+            var $this = $(this);
+            if (isValueEmpty($this) || isValueEqualToPlaceholder($this)) {
+                setValueEqualToPlaceholder($(this));
+                addPlaceholderClass($this);
+            }
+        });
+    }
+    function removePlaceholdersOnSubmit() {
+        placeholders.closest('form').submit(function () {
+            var $this = $(this);
+            $this.find('[placeholder]').each(function () {
                 if (isValueEqualToPlaceholder($this)) {
                     clearValue($this);
                 }
             });
         });
     }
-    function setValueEqualToPlaceholder($input) {
-        var placeholderValue = $input.attr('placeholder');
-        $input.val(placeholderValue);
+
+    if (!hasNativePlaceholderSupport) {
+        applyPlaceholdersOnLoad();
+        handleFocusAndBlurEvents();
+        removePlaceholdersOnSubmit();
+        hasAppliedFallback = true;
     }
 }
-$(function() {
+$(function () {
+    "use strict";
     applyPlaceholders();
 });
